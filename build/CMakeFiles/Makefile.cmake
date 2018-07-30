96707b306a89f7b360338c3e1a1490afe6120109
ahuizxc <shenghui.liu@intel.com>
2018-07-30 11:26:38 +0800

    0730

--- build/CMakeFiles/Makefile.cmake
+++ build/CMakeFiles/Makefile.cmake
@@ -249,5 +249,6 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
+  "src/CMakeFiles/detectFeatures.dir/DependInfo.cmake"
   "src/CMakeFiles/slambase.dir/DependInfo.cmake"
   )
--- src/detectFeatures.cpp
+++ src/detectFeatures.cpp
@@ -18,24 +18,20 @@ using namespace std;
 int main( int argc, char** argv )
 {
     // 声明并从data文件夹里读取两个rgb与深度图
-    cv::Mat rgb1 = cv::imread( "./data/rgb1.png");
-    cv::Mat rgb2 = cv::imread( "./data/rgb2.png");
-    cv::Mat depth1 = cv::imread( "./data/depth1.png", -1);
-    cv::Mat depth2 = cv::imread( "./data/depth2.png", -1);
+    cv::Mat rgb1 = cv::imread( "/home/intel/rgb2cld/data/rgb1.png");
+    cv::Mat rgb2 = cv::imread( "/home/intel/rgb2cld/data/rgb2.png");
+    cv::Mat depth1 = cv::imread( "/home/intel/rgb2cld/data/depth1.png", -1);
+    cv::Mat depth2 = cv::imread( "/home/intel/rgb2cld/data/depth2.png", -1);
 
     // 声明特征提取器与描述子提取器
-    cv::Ptr<cv::FeatureDetector> detector;
-    cv::Ptr<cv::DescriptorExtractor> descriptor;
-
+    cv::Ptr<cv::Feature2D> detector = cv::ORB::create();
+   
     // 构建提取器，默认两者都为 ORB
     
     // 如果使用 sift, surf ，之前要初始化nonfree模块
     // cv::initModule_nonfree();
     // _detector = cv::FeatureDetector::create( "SIFT" );
     // _descriptor = cv::DescriptorExtractor::create( "SIFT" );
-    
-    detector = cv::FeatureDetector::create("ORB");
-    descriptor = cv::DescriptorExtractor::create("ORB");
 
     vector< cv::KeyPoint > kp1, kp2; //关键点
     detector->detect( rgb1, kp1 );  //提取关键点
@@ -47,13 +43,13 @@ int main( int argc, char** argv )
     cv::Mat imgShow;
     cv::drawKeypoints( rgb1, kp1, imgShow, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS );
     cv::imshow( "keypoints", imgShow );
-    cv::imwrite( "./data/keypoints.png", imgShow );
-    cv::waitKey(0); //暂停等待一个按键
+    cv::imwrite( "/home/intel/rgb2cld/data/keypoints.png", imgShow );
+    //cv::waitKey(0); //暂停等待一个按键
    
     // 计算描述子
     cv::Mat desp1, desp2;
-    descriptor->compute( rgb1, kp1, desp1 );
-    descriptor->compute( rgb2, kp2, desp2 );
+    detector->compute( rgb1, kp1, desp1 );
+    detector->compute( rgb2, kp2, desp2 );
 
     // 匹配描述子
     vector< cv::DMatch > matches; 
@@ -65,8 +61,8 @@ int main( int argc, char** argv )
     cv::Mat imgMatches;
     cv::drawMatches( rgb1, kp1, rgb2, kp2, matches, imgMatches );
     cv::imshow( "matches", imgMatches );
-    cv::imwrite( "./data/matches.png", imgMatches );
-    cv::waitKey( 0 );
+    cv::imwrite( "/home/intel/rgb2cld/data/matches.png", imgMatches );
+    ///cv::waitKey( 0 );
 
     // 筛选匹配，把距离太大的去掉
     // 这里使用的准则是去掉大于四倍最小距离的匹配
@@ -89,9 +85,8 @@ int main( int argc, char** argv )
     cout<<"good matches="<<goodMatches.size()<<endl;
     cv::drawMatches( rgb1, kp1, rgb2, kp2, goodMatches, imgMatches );
     cv::imshow( "good matches", imgMatches );
-    cv::imwrite( "./data/good_matches.png", imgMatches );
-    cv::waitKey(0);
-
+    cv::imwrite( "/home/intel/rgb2cld/data/good_matches.png", imgMatches );
+    
     // 计算图像间的运动关系
     // 关键函数：cv::solvePnPRansac()
     // 为调用此函数准备必要的参数
@@ -130,13 +125,14 @@ int main( int argc, char** argv )
         {0, C.fy, C.cy},
         {0, 0, 1}
     };
+  
 
     // 构建相机矩阵
     cv::Mat cameraMatrix( 3, 3, CV_64F, camera_matrix_data );
     cv::Mat rvec, tvec, inliers;
     // 求解pnp
-    cv::solvePnPRansac( pts_obj, pts_img, cameraMatrix, cv::Mat(), rvec, tvec, false, 100, 1.0, 100, inliers );
-
+    cv::solvePnPRansac( pts_obj, pts_img, cameraMatrix, cv::Mat(), rvec, tvec, false, 100, 8, 0.99, inliers );
+    cv::waitKey(0);
     cout<<"inliers: "<<inliers.rows<<endl;
     cout<<"R="<<rvec<<endl;
     cout<<"t="<<tvec<<endl;
@@ -149,7 +145,7 @@ int main( int argc, char** argv )
     }
     cv::drawMatches( rgb1, kp1, rgb2, kp2, matchesShow, imgMatches );
     cv::imshow( "inlier matches", imgMatches );
-    cv::imwrite( "./data/inliers.png", imgMatches );
+    cv::imwrite( "/home/intel/rgb2cld/data/inliers.png", imgMatches );
     cv::waitKey( 0 );
 
     return 0;
